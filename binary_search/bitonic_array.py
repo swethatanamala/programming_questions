df2497384acfa3b78d417d1a7a9104bfb6baa7e4
Swetha Tanamala <swethatanamala@QURELAP-123.local>
2023-01-20 13:30:19 +0530

    solved bitonic array problem

--- binary_search/bitonic_array.py
+++ binary_search/bitonic_array.py
@@ -13,7 +13,43 @@ def get_index(arr: list[int]):
             else:
                 right = mid
     
-arr = [3, 9, 10, 20, 17, 5, 1]
-ind = get_index(arr)
-result = arr[:ind] + arr[ind:][::-1]
-print(result)
+def find(arr, search):
+    left, right = 0, n
+    while left < right:
+        mid = (left + right) // 2
+        if arr[mid] == search:
+            return mid
+        elif arr[mid] < search:
+            left = mid + 1
+        else:
+            right = mid
+    return -1
+
+arr = [5, 6, 7, 8, 9, 10, 3, 2, 1]
+search = -1
+ind, n = get_index(arr), len(arr)
+
+if ind == 0:
+    arr = arr[::-1]
+elif ind < n - 1:
+    i, j = ind, ind + 1
+    tmp = arr[j]
+    while (i >= -1) and (j < n):
+        if (i >= 0) and (arr[i] > tmp):
+            i -= 1
+        else:
+            k = i + 1
+            place1 = arr[k]
+            while k < j:
+                place2 = arr[k + 1]
+                arr[k + 1] = place1
+                place1 = place2
+                k += 1
+            arr[i + 1] = tmp
+            j += 1
+            if j == n:
+                break
+            tmp = arr[j]
+print(arr, search)
+print(find(arr, search))
+
